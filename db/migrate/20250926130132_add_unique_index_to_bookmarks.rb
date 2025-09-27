cc8bd195d28c4d1493021b42adc648febbe66391
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-09-27 10:50:22 +0200

    devise setup

--- db/migrate/20250926130132_add_unique_index_to_bookmarks.rb
+++ db/migrate/20250926130132_add_unique_index_to_bookmarks.rb
@@ -1,4 +1,6 @@
+# Prevents users bookmarking the same output more than once
 class AddUniqueIndexToBookmarks < ActiveRecord::Migration[7.1]
   def change
+    add_index :bookmarks, %i[user_id output_id], unique: true
   end
 end
