9e7f5fc8309854102b0ecc433125ed43787d4391
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-09-27 13:30:52 +0200

    created the bookmark, output and user model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :output
+
+  validates :user_id, uniqueness: { scope: :output_id }
 end
--- app/models/output.rb
+++ app/models/output.rb
@@ -1,2 +1,6 @@
 class Output < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :users, through: :bookmarks
+
+  validates :city, :country, :lat, :lng, :text, presence: true
 end
