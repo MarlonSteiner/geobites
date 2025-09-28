047e387671c856fcaccb020967d969cacdf63660
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-09-28 16:10:37 +0200

    Remove Devise template secret/pepper examples

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,6 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'example'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +125,6 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = 'example'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
