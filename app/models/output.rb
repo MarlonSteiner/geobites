4d7e53d5f5b82056697dd18ff6351a45b452735a
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-10-06 00:28:19 +0200

    got prompting working and am starting with geocoding addlistener to fact generation synching

--- app/models/output.rb
+++ app/models/output.rb
@@ -10,4 +10,12 @@ class Output < ApplicationRecord
   def address
     "#{city}, #{country}"
   end
+  # Output prompting template from LeWagon
+  def content
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+    })
+    return chatgpt_response["choices"][0]["message"]["content"]
 end
