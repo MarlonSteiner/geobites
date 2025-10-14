889bc809666c66e96c84d9d6a615712728aa2da6
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-10-14 02:51:01 +0200

    bookmark still in progress

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: %i[new create]
+  before_action :authenticate_user!
 
   # https://medium.com/@tmtushar28/build-a-bookmark-app-in-rails-from-scratch-part-1-b9455b72a70b
   # We are able to have a database specific to every user thanks to Devise. And we pass the data from output to bookmark which is all the data we need
@@ -14,6 +13,7 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = current_user.bookmarks.create(output_id: params[:output_id])
+    head :ok
   end
 
   def destroy
