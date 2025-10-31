46ef242c400b81fb47f82d90482e3cb2ec0287d1
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-10-31 22:31:51 +0100

    bookmarks works and displays array on the bookmarks page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   # https://medium.com/@tmtushar28/build-a-bookmark-app-in-rails-from-scratch-part-1-b9455b72a70b
   # We are able to have a database specific to every user thanks to Devise. And we pass the data from output to bookmark which is all the data we need
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,29 @@
 class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:select]
 
+  def index
+  end
+
   def select
     # Make the country and city variables
     country = params[:country]
     city = params[:city]
+    latitude = params[:latitude]
+    longitude = params[:longitude]
 
     # Create output variable and use OpenAI service
     output = OpenaiService.generate_fact(city, country)
 
     # Output instance which is the easiest way I know to store and send a hash of data
-    @output = Output.create(city: city, country: country, text: output)
+    @output = Output.create( city: city, country: country, text: output, latitude: latitude, longitude: longitude )
+
+    # Error handling time
+    if @output.persisted?
+      puts "Output saved successfully! ID: #{@output.id}"
+    else
+      puts "output failed to save"
+      puts "Errors: #{@output.errors.full_messages}"
+    end
 
     # Kept making the mistake rendering an instance...
     # We are sending back both the text & output_id
